2c7cae8c4bff394336d603145a25c308cf97c094
Dick-Tator <142325460+Dick-Tator@users.noreply.github.com>
2025-05-06 20:57:12 +0200

    Update basics.js

--- Buchhaltung/static/basics.js
+++ Buchhaltung/static/basics.js
@@ -1,9 +1,14 @@
+/**
+* Diese Funktion wird immer dann aufgerufen, wenn ein Inputfeld geöffnet wird
+* @params {id} -Id, des Elements welches geöffnet werden soll
+* 
+*/
 function ToggleInput(pressedId){
-    const selectedInput  = document.getElementById(pressedId);
-    if(selectedInput.style.display == "block"){
-        document.querySelectorAll(".input").forEach(item => {item.style.display = "none"});
+    const selectedInput  = document.getElementById(pressedId); //hol das zu öffnende Element
+    if(selectedInput.style.display == "block"){ //wenn Das Element breits geöffnet ist
+        document.querySelectorAll(".input").forEach(item => {item.style.display = "none"}); // schliesse alle Input Felder
         return;
     }
-    document.querySelectorAll(".input").forEach(item => {item.style.display = "none"});
-    selectedInput.style.display = "block";
+    document.querySelectorAll(".input").forEach(item => {item.style.display = "none"}); //scliesse alle Input Felder
+    selectedInput.style.display = "block"; //öffne das eine Input Feld
 }
